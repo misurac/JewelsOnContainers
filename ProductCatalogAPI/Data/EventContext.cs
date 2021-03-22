c47d0f6adf9b8e43457df517239451f4671c6866
misurac <rmisurac@gmail.com>
2021-03-21 19:21:31 -0500

    Module 10

---
+++ ProductCatalogAPI/Data/EventContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductCatalogAPI.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductCatalogAPI.Data
+{
+    public class EventContext : DbContext
+    {
+        public EventContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<EventType> EventTypes { get; set; }
+        public DbSet<Event> Events { get; set; }
+    }
+}
