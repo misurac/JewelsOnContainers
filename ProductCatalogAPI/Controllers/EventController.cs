1eaf76d3f3d4a5772ad902d092d1fbdb4a9f7f0a
misurac <rmisurac@gmail.com>
2021-03-23 22:14:09 -0500

    End of module 13

---
+++ ProductCatalogAPI/Controllers/EventController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProductCatalogAPI.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductCatalogAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventController : ControllerBase
+    {
+        private readonly EventContext _context;
+        public EventController(EventContext context)
+        {
+            _context = context;
+        }
+        [HttpGet("[action]")]
+        public async Task<IActionResult> Events(
+            [FromQuery]int pageIndex = 0, 
+            [FromQuery]int pageSize = 2)
+        {
+            var events = await _context.Events
+                .OrderBy(e => e.Name)
+                .Skip(pageIndex * pageSize)
+                .Take(pageSize)
+                .ToListAsync();
+        }
+    }
+}
