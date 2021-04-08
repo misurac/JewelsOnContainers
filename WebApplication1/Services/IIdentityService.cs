348e6bcf6d62dfe2da2fb8dd917dd8cbe503649f
misurac <rmisurac@gmail.com>
2021-04-07 22:08:46 -0500

    through lecture 16

---
+++ WebApplication1/Services/IIdentityService.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebMvc1.Models;
+
+namespace WebMvc1.Services
+{
+    public interface IIdentityService
+    {
+        Task<EventPageInfo> GetCatalogItemsAsync(int page, int size, int? brand, int? type);
+        Task<IEnumerable<SelectListItem>> GetBrandsAsync();
+        Task<IEnumerable<SelectListItem>> GetTypesAsync();
+    }
+}
