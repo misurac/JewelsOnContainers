7b7adf303a904d7e4347401eb76197b8b37d0ffc
misurac <rmisurac@gmail.com>
2021-04-20 20:26:58 -0500

    built more of cart api

---
+++ WebMvc1/Services/ICartService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebMvc1.Models;
+
+namespace WebMvc1.Services
+{
+    interface ICartService
+    {
+        Task<Cart> GetCart(ApplicationUser user);
+        Task AddItemToCart(ApplicationUser user, CartItem product);
+        Task<Cart> SetQuantities(ApplicationUser user, Dictionary<string, int> quantities);
+        Task ClearCart(ApplicationUser user);
+    }
+}
